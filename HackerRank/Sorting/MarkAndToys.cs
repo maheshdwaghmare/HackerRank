85994d2fee8cc8589f8c310b011fb0cdd08e8eb6
Mahesh ED <Mahesh.Waghmare@Effective.Digital>
2019-11-15 15:57:49 +0530

    Added Mark and Toys Implementation.

---
+++ HackerRank/Sorting/MarkAndToys.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class MarkAndToys
+    {
+        // Complete the maximumToys function below.
+        static int maximumToys(int[] prices, int k)
+        {
+            Array.Sort(prices);
+            int i = 0;
+            while (k - prices[i] >= 0)
+            {
+                k = k - prices[i];
+                i++;
+            }
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] prices = Array.ConvertAll(Console.ReadLine().Split(' '), pricesTemp => Convert.ToInt32(pricesTemp));
+            int result = maximumToys(prices, k);
+
+            Console.WriteLine(result);
+            //textWriter.WriteLine(result);
+
+            Console.ReadKey();
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
