575555324d9e8a8db1ac2acaa03b5c23232e8398
Mahesh ED <Mahesh.Waghmare@Effective.Digital>
2019-11-16 14:45:49 +0530

    Implementation of Count Triplets.

---
+++ HackerRank/DictionariesHashmaps/CountTriplets.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class CountTriplets
+    {
+        // Complete the countTriplets function below.
+        static long countTriplets(List<long> arr, long r)
+        {
+            long triplets = 0;
+            Dictionary<long, long> dict = new Dictionary<long, long>();
+            Dictionary<long, long> dictPairs = new Dictionary<long, long>();
+
+            for (int index = arr.Count - 1; index >= 0; index--)
+            {
+                if (dictPairs.ContainsKey(arr[index] * r))
+                    triplets += dictPairs[arr[index] * r];
+
+                if (dict.ContainsKey(arr[index] * r))
+                {
+                    if (dictPairs.ContainsKey(arr[index]))
+                        dictPairs[arr[index]] = dictPairs[arr[index]] + dict[arr[index] * r];
+                    else
+                        dictPairs[arr[index]] = dict[arr[index] * r];
+                }
+
+                if (!dict.ContainsKey(arr[index]))
+                    dict.Add(arr[index], 1);
+                else
+                    dict[arr[index]]++;
+            }
+
+            return triplets;
+        }
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nr = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(nr[0]);
+
+            long r = Convert.ToInt64(nr[1]);
+
+            List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+
+            long ans = countTriplets(arr, r);
+
+            Console.WriteLine(ans);
+            //textWriter.WriteLine(ans);
+
+            Console.ReadKey();
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
--- HackerRank/DictionariesHashmaps/TwoStrings.cs
+++ HackerRank/DictionariesHashmaps/TwoStrings.cs
@@ -30,27 +30,27 @@ namespace HackerRank
 
             return "No";
         }
-        static void Main(string[] args)
-        {
-            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        //static void Main(string[] args)
+        //{
+        //    //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            int q = Convert.ToInt32(Console.ReadLine());
+        //    int q = Convert.ToInt32(Console.ReadLine());
 
-            for (int qItr = 0; qItr < q; qItr++)
-            {
-                string s1 = Console.ReadLine();
+        //    for (int qItr = 0; qItr < q; qItr++)
+        //    {
+        //        string s1 = Console.ReadLine();
 
-                string s2 = Console.ReadLine();
+        //        string s2 = Console.ReadLine();
 
-                string result = twoStrings(s1, s2);
+        //        string result = twoStrings(s1, s2);
 
-                //textWriter.WriteLine(result);
-                Console.WriteLine(result);
-            }
+        //        //textWriter.WriteLine(result);
+        //        Console.WriteLine(result);
+        //    }
 
-            //textWriter.Flush();
-            //textWriter.Close();
-            Console.ReadKey();
-        }
+        //    //textWriter.Flush();
+        //    //textWriter.Close();
+        //    Console.ReadKey();
+        //}
     }
 }
